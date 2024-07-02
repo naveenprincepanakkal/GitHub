15f0c085634f8a0a7a6b64d82dbee4942331afe8
naveen <naveenprincep@gmail.com>
2024-07-02 19:53:10 +0530

    ui theme updates

--- app/src/main/java/com/naveenprince/github/MainActivity.kt
+++ app/src/main/java/com/naveenprince/github/MainActivity.kt
@@ -3,8 +3,11 @@ package com.naveenprince.github
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.naveenprince.github.ui.GitHubUsersApp
@@ -20,15 +23,19 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             GitHubUsersTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    GitHubUsersApp()
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Surface(
+                        modifier = Modifier.fillMaxSize().padding(innerPadding),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        GitHubUsersApp()
+                    }
                 }
             }
         }
     }
 }
+
