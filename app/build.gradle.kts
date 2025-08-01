2fcc877d1655ab2238270c9eead46f4b1e610d26
naveen <naveenprincep@gmail.com>
2025-08-01 13:02:06 +0530

    updating gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,14 +37,15 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+}
+
+kotlin {
+    jvmToolchain(17)
 }
 
 dependencies {
